1b30f4c9dd5a30d45bdd04da1e32aeb2a797345d
Codie Aragon <codiearagon10@gmail.com>
2026-02-19 12:40:03 -0800

    change to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Router } from 'react-router-dom'
+import { Route, createHashRouter, createRoutesFromElements, RouterProvider, Router } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import CapstoneLanding from "./pages/Capstone/CapstoneLanding"
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/capstone' element={<CapstoneLanding />} />
     </Route>
-  ),
-  {
-    basename: "/portfolio/"
-  }
+  )
 );
 
 const App = () => {
