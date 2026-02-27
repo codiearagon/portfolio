d974d8bfcdc76621e90157e1501e788ecb80c34d
Codie Aragon <codiearagon10@gmail.com>
2026-02-27 09:48:26 -0800

    change experience page little bit

--- src/pages/Capstone/CapstoneExperience.jsx
+++ src/pages/Capstone/CapstoneExperience.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const CapstoneExperience = () => {
   return (
-    <div>CapstonExperience</div>
+    <div>
+      <p>theme 1 (lead to another page)</p>
+      <p>theme 2 (lead to another page)</p>
+      <p>theme 3 (lead to another page)</p>
+      <p>theme 4 (lead to another page)</p>
+    </div>
   )
 }
 
